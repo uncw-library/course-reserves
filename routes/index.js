c8db570773ca0cf955becf63e3e98da26c2ec2a4
ElliotSimpson <simpsonj@uncw.edu>
2018-07-05 16:32:49 -0400

    add call number

--- routes/index.js
+++ routes/index.js
@@ -9,7 +9,8 @@ router.get('/', function(req, res, next) {
   
   const sql = "" +
 
-  "Select distinct sierra_view.course_record_item_record_link.item_record_id,varfield_view.record_num,title,content,field_content,sierra_view.bib_view.record_num as bibrecord, call_number,item_view.location_code,location_name.name as location,item_status_property_myuser.name as status " +
+  "Select distinct sierra_view.course_record_item_record_link.item_record_id,varfield_view.record_num,title,content,field_content,sierra_view.bib_view.record_num as bibrecord, " +
+  "(REPLACE(REPLACE(item_record_property.call_number,\'|a\',\'\'),\'|b\',\' \')) as call_number,item_view.location_code,location_name.name as location,item_status_property_myuser.name as status " +
   "from sierra_view.course_record " +
   
   "LEFT JOIN  sierra_view.course_record_item_record_link " +
